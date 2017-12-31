858e01f1e1fb26fac0e400c7810eda07a246a290
Ben Allred <skuttleman@gmail.com>
2017-12-31 13:33:22 -0700

    fixed file directory names '-' -> '_'

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject com.ben-allred/collaj "0.1.0"
+(defproject com.ben-allred/collaj "0.2.0"
     :description "A state management system for Clojure/ClojureScript based on Redux"
     :url "https://www.github.com/skuttleman/collaj"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
     :min-lein-version "2.6.1"
     :source-paths ["src/cljc"]
+    :test-paths ["test/cljs"]
     :dependencies [[org.clojure/clojure "1.8.0"]
                    [org.clojure/clojurescript "1.9.542"]]
     :plugins [[lein-cljsbuild "1.1.6"]]
